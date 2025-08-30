183e9d51656873ca09318fa47b0f17b074ed0ae9
sa1998ma <sama.fahandej@gmail.com>
2025-08-30 09:59:58 +0330

    Remove test files git1.py and main.py

--- git1.py
+++
@@ -1,6 +0,0 @@
-print("hello git")
-print("Wowwww, it is my first change!!!!!!!!")
-print("resgfesrytr6htrhgrfdgrgdrfg")
-
-print("hi again, wow is it going?")
-
--- main.py
+++
@@ -1,6 +0,0 @@
-print("helloooooooo world!")
-print("Heyyyyy I am here, be happyyyyy!!!!")
-print("srfgzdrgdrzefhgtfvjgyfjbhygjkmyhjgt")
-
-print("hi again, wow is it going?")
-
